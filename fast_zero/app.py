0d75dad2c4387436d10f4bf97819b8828c9ee7c8
Diogo Caribé <dogosousa@gmail.com>
2024-06-25 12:56:24 -0300

    Realizando o delete.

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,7 +2,7 @@
 
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 from fast_zero.schemas import Message, UserDb, UserList, UserPublic, UserSchema
 
@@ -29,5 +29,25 @@ def create_user(user: UserSchema):  # Schema pydantic do user e validação
 
 
 @app.get('/users/', response_model=UserList)
-def read_user():
+def read_users():
     return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+    user_with_id = UserDb(id=user_id, **user.model_dump())
+
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete('/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found')
+
+    del database[user_id - 1]
+    return {'message': 'User deleted'}
--- tests/test_app.py
+++ tests/test_app.py
@@ -27,12 +27,50 @@ def test_create_user(client):
 
 def test_read_users(client):
     response = client.get('/users/')
-    lista_users = {'users': [
-        {
-            'id': 1,
-            'username': 'usernameteste',
-            'email': 'emailteste@test.com',
-        }
-    ]}
+    lista_users = {
+        'users': [
+            {
+                'id': 1,
+                'username': 'usernameteste',
+                'email': 'emailteste@test.com',
+            }
+        ]
+    }
     assert response.status_code == HTTPStatus.OK
     assert response.json() == lista_users
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': '123',
+            'username': 'nematest',
+            'email': 'teste@teste.com',
+            'id': 1,
+        },
+    )
+
+    # assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {
+        'username': 'nematest',
+        'email': 'teste@teste.com',
+        'id': 1,
+    }
+
+
+def test_update_user_id_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'password': '123',
+            'username': 'nematest',
+            'email': 'teste@teste.com',
+            'id': 1,
+        },
+    )
+
+    # assert response.status_code == HTTPStatus.OK
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
