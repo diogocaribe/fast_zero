a2f46c8ced267e97660fd45afc6b6819c9309cbf
Diogo Caribé <dogosousa@gmail.com>
2024-06-25 10:20:35 -0300

    Adicionando o test em create_user.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
@@ -9,3 +11,24 @@ def test_read_root_deve_retornar_ok_ola_mundo():
     response = client.get('/')  # Act (Ação)
     # Afirmação (garantir)
     assert response.json() == {'message': 'Olá mundo'}
+
+
+def test_create_user():
+    # Fases do test
+    client = TestClient(app)  # Arragen (organização)
+    # Act (Ação)
+    # Afirmação (garantir)
+    user_test = {
+        'username': 'usernameteste',
+        'email': 'emailteste@test.com',
+        'password': 'password',
+    }
+
+    user_response = {
+        'id': 1,
+        'username': 'usernameteste',
+        'email': 'emailteste@test.com',
+    }
+    response = client.post('/user/', json=user_test)
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == user_response
