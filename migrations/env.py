03f391b829c53a96a5cb3d6350923772d0c86f92
Diogo Caribé <dogosousa@gmail.com>
2024-06-27 10:51:40 -0300

    Ajustando linhas do arquivo de configuração do alembic (migration)

--- migrations/env.py
+++ migrations/env.py
@@ -3,8 +3,8 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from fast_zero.settings import Settings
 from fast_zero.models import table_registry
+from fast_zero.settings import Settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
