f9538bd3ba7e7480b095b43b6e0614d0795f81d1
Diogo Caribé <dogosousa@gmail.com>
2024-06-29 07:46:56 -0300

    refactor: configurando as fixture de maneira que seja utilizado o banco na memória para os testes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,23 @@
 import pytest
 from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
+from sqlalchemy import StaticPool, create_engine
 from sqlalchemy.orm import Session
 
 from fast_zero.app import app
+from fast_zero.database import get_session
 from fast_zero.models import table_registry
 
 
 @pytest.fixture()
-def client():
-    return TestClient(app)
+def client(session):
+    def get_session_override():
+        return session
+
+    with TestClient(app) as client:
+        app.dependency_overrides[get_session] = get_session_override
+        yield client
+
+    app.dependency_overrides.clear()
 
 
 @pytest.fixture()
@@ -17,7 +25,11 @@ def session():
     # TODO Construir a session para realiazr os testes.
     # Tem de fazer o modelo e que ele esteja visível para create table
 
-    engine = create_engine('sqlite://')
+    engine = create_engine(
+        'sqlite://',
+        connect_args={'check_same_thread': False},
+        poolclass=StaticPool,
+    )
     table_registry.metadata.create_all(engine)
 
     with Session(engine) as session:
