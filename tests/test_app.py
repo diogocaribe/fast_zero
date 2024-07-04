001d6026bd72a3cfd934ab724ce02707944de67d
Diogo Caribé <dogosousa@gmail.com>
2024-07-04 16:42:09 -0300

    Adicionando o teste para token invalido

--- tests/test_app.py
+++ tests/test_app.py
@@ -145,3 +145,12 @@ def test_get_token(client, user):
     assert token['token_type'] == 'Bearer'
     # Testando se existe o access_token
     assert 'access_token' in token
+
+
+def test_jwt_invalid_token(client):
+    response = client.delete(
+        '/users/1', headers={'Authorization': 'Bearer token-invalido'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
